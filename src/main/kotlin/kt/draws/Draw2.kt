64230d87e2f27c9f4ef6471d29082d61f0166f3a
iguigui <atmzxl@163.com>
2022-12-12 18:32:54 +0800

    水提交

--- src/main/kotlin/kt/draws/Draw2.kt
+++ src/main/kotlin/kt/draws/Draw2.kt
@@ -2,34 +2,85 @@ package kt.draws
 
 import java.awt.Color
 import java.awt.Font
+import java.awt.FontMetrics
 import java.awt.RenderingHints
 import java.awt.font.TextLayout
 import java.awt.image.BufferedImage
+import java.awt.image.BufferedImage.TYPE_INT_RGB
 import java.io.File
 import javax.imageio.ImageIO
 
 
 fun main() {
-    val image: BufferedImage = ImageIO.read(
-        File("D:\\bot\\template.png")
+
+    var str = "龙王排行榜\n" +
+            "今天是2022-12-12日，今年的第346天，您的2022年使用进度条：\n" +
+            "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░ 94.79% \n" +
+            "本群2022-12-12日消息总量：187条\n" +
+            "第1名：ForeverNuclear ，59条消息\n" +
+            "第2名：ncturneon ，44条消息\n" +
+            "第3名：我的龟龟🐢🐢，31条消息\n" +
+            "第4名：中森榨菜(快递难产版) ，29条消息\n" +
+            "第5名：我凭什么受啊我！ ，10条消息\n" +
+            "第6名：卢小白 ，7条消息\n" +
+            "第7名：书桓 ，5条消息\n" +
+            "第8名：床垫被分解了分线完成、该去完成主线任务了 ，1条消息\n" +
+            "第9名：真的我宁可听你唱你干嘛哈哈哎哟 ，1条消息"
+
+
+    var strList : List<String> = arrayListOf(
+        "测试测试我测试测试我测试测试",
+        "测试测试我",
+        "测试",
+        "测试测试我",
+        "测试测试我",
+        "123456789",
+        "ABCDIIIILLLiii",
+        "测试测试我测试测试我测试测试",
+        "iiii1111..??``"
     )
 
-    val g = image.createGraphics()
-    val deriveFont = Font.createFont(Font.TRUETYPE_FONT, File("D:\\font\\SourceHanSansSC-Light-2.otf")).deriveFont(60f)
-    g.font = deriveFont
-    g.color = Color.BLACK
+    strList = str.split("\n")
 
-//    val txt = JTextArea()
-//    txt.font = Font.createFont(Font.TRUETYPE_FONT, File("D:\\font\\SourceHanSansCN-VF-2.otf")).deriveFont(20f)
-//    txt.foreground = Color.black
+    var fontSize = 80f
+
+    val deriveFont =
+        Font.createFont(Font.TRUETYPE_FONT, File("D:\\font\\SourceHanSansHWSC-Regular.otf")).deriveFont(fontSize)
+    val fontMetrics = BufferedImage(1, 1, TYPE_INT_RGB).createGraphics().getFontMetrics(deriveFont)
+
+    var width = strList.map { it.width(fontMetrics) }.maxByOrNull { it } ?: 0
+    width += (fontSize * 2f).toInt()
+    val height = (strList.size * fontSize * 1.5 + fontSize).toInt()
+
+
+
+    var image = BufferedImage(width, height, TYPE_INT_RGB)
+    val g = image.createGraphics()
+    g.font = deriveFont
+
+    //fill as white
+    g.fillRect(0, 0, width, height)
+
+    g.color = Color.BLACK
 
     g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
     g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
-    val tl = TextLayout("测试测试我的龟龟我的测试测试我的龟龟我的测试测试我的龟龟我的测试测试我的龟龟我的", deriveFont, g.fontRenderContext)
-//    g.drawString("测试测试我的龟龟我的", 200, 200)
-    tl.draw(g, 200f, 200f)
+    var tmpY = fontSize * 1.5f
+    var yStep = fontSize * 1.5f
+    strList.forEach {
+        val tl = TextLayout(it, deriveFont, g.fontRenderContext)
+        tl.draw(g, fontSize, tmpY)
+        tmpY += yStep
+//        yStep += 10
+    }
+
     g.dispose()
 
     ImageIO.write(image, "png", File("D:\\bot\\2.png"))
 
+}
+
+
+fun String.width(fm: FontMetrics): Int {
+    return fm.stringWidth(this)
 }
