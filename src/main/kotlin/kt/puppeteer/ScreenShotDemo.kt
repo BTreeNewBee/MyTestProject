a451034a48a521e86afbebe6f6f4e903da2e0e9a
iguigui <atmzxl@163.com>
2022-12-21 09:18:26 +0800

    puppeteer

--- src/main/kotlin/kt/puppeteer/ScreenShotDemo.kt
+++ src/main/kotlin/kt/puppeteer/ScreenShotDemo.kt
@@ -4,10 +4,7 @@ import com.ruiyun.jvppeteer.core.Puppeteer
 import com.ruiyun.jvppeteer.core.browser.Browser
 import com.ruiyun.jvppeteer.core.browser.BrowserFetcher
 import com.ruiyun.jvppeteer.core.page.Page
-import com.ruiyun.jvppeteer.options.Clip
-import com.ruiyun.jvppeteer.options.LaunchOptions
-import com.ruiyun.jvppeteer.options.LaunchOptionsBuilder
-import com.ruiyun.jvppeteer.options.ScreenshotOptions
+import com.ruiyun.jvppeteer.options.*
 
 
 fun main() {
@@ -20,12 +17,14 @@ fun main() {
     val page: Page = browser.newPage()
 //    page.goTo("https://www.baidu.com/?tn=98012088_10_dg&ch=3")
     page.goTo("D:\\nodejs\\ImageServer\\sort.html")
-    
+    //set page size
+    page.setViewport(Viewport(500, 800, 2.0, false, false, false))
+
+
 
     val screenshotOptions = ScreenshotOptions()
     //设置截图范围
-    //设置截图范围
-    val clip = Clip(1.0, 1.56, 1024.0, 1024.0)
+    val clip = Clip(1.0, 1.56, 500.0, 800.0)
     screenshotOptions.clip = clip
     //设置存放的路径
     screenshotOptions.path = "test.png"
